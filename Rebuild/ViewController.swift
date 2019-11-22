8d2f9a3b020f704de6caea0c6d47efc85ea465f6
Steven Hertz <hertzsteven@icloud.com>
2019-11-22 07:24:41 -0500

    Initial Commit

---
+++ Rebuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rebuild
+//
+//  Created by Steven Hertz on 11/22/19.
+//  Copyright © 2019 DevelopItSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
